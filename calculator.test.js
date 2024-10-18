07e77ffe838a960058a10f3c09fb199e348741f1
Krishna Ahire <krishnaahire24@gmail.com>
2024-10-18 17:11:20 +0530

    added test case for single number

--- calculator.test.js
+++ calculator.test.js
@@ -1,8 +1,10 @@
-
 describe("String Calculator", () => {
-
   test("return 0 for empty string", () => {
     expect(add("")).toBe(0);
   });
 
+  test("should return the number itself for a single number", () => {
+    expect(add("1")).toBe(1);
+  });
+  
 });
