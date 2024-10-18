792325a062f14cebdf7c2d1c117bfa2beaa468af
Krishna Ahire <krishnaahire24@gmail.com>
2024-10-18 17:56:27 +0530

    handled '\n' delimeters

--- calculator.js
+++ calculator.js
@@ -3,5 +3,10 @@ const add = (numbers) => {
     if(numbers == ""){
         return 0
     }
+ 
+    let delimiter = ","
+    let number_str = numbers
+
+    number_str = number_str.replace(/\n/g, delimiter)
 
 }
